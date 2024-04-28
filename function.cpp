a82caf83745027803703da43ae4e66fb90b947c4
SadiyaAyj <122111052+SadiyaAyj@users.noreply.github.com>
2024-04-28 22:33:56 +0530

    Create function.cpp

---
+++ function.cpp
@@ -0,0 +1,20 @@
+//program to print 8 assignment operator using function.
+#include<iostream>
+using namespace std;
+void assign();
+int main()
+{
+	int i;
+	
+		assign();
+		cout<<"IUST"<<endl;
+		assign();
+		cout<<"abcd"<<endl;
+
+}
+void assign(){
+	for(int i=0;i<=8;i++){
+		cout<<"=";
+}
+     cout<<endl;
+}
